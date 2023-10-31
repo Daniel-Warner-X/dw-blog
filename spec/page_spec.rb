6896c82cdc4be92cc2ce75a92dddd52464c6d2e3
Daniel Warner <dan@danielwarner.net>
2023-10-31 15:44:37 -0400

    Update page_spec.rb
    simplify test

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -7,8 +7,5 @@ $test_pages.each do |test_page|
         it 'Renders Properly' do
             expect(page.status_code).to eq(200)
         end
-        it 'has footer' do
-            expect(page).to have_css('footer')
-        end
     end
 end
